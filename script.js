3155d49c22d09f7a2333cfee6233317c0c6649dc
Mario Hernandez Yañez <maheya@MacBook-Pro-de-Mario.local>
2022-11-12 09:17:12 -0600

    proyecto terminado

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+
 function switchTheme(event){
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme','dark');
+        localStorage.setItem('theme','dark');
         modoOscuro();
     }else{
         document.documentElement.setAttribute('data-theme','light');
+        localStorage.setItem('theme','light');
         modoClaro();
     }
 }
@@ -24,9 +27,8 @@ function modoOscuro(){
     toggleIcon.children[0].textContent = 'Modo Oscuro';
     toggleIcon.children[1].classList.remove('fa-sun');
     toggleIcon.children[1].classList.add('fa-moon');
-    image1.src = 'img/undraw_proud_coder_dark.svg';
-    image2.src = 'img/undraw_feeling_proud_dark.svg';
-    image3.src = 'img/undraw_conceptual_idea_dark.svg';
+    imagen('dark');
+
 }
 
 function modoClaro(){
@@ -35,10 +37,29 @@ function modoClaro(){
     toggleIcon.children[0].textContent = 'Modo Claro';
     toggleIcon.children[1].classList.remove('fa-moon');
     toggleIcon.children[1].classList.add('fa-sun');
-    image1.src = 'img/undraw_proud_coder_light.svg';
-    image2.src = 'img/undraw_feeling_proud_light.svg';
-    image3.src = 'img/undraw_conceptual_idea_light.svg';
+    imagen('light');
 }
 
+function imagen(tipo){
 
+    image1.src = `img/undraw_proud_coder_${tipo}.svg`;
+    image2.src = `img/undraw_feeling_proud_${tipo}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${tipo}.svg`;
+
+}
+
+// evento listener
 toogleSwitch.addEventListener("change", switchTheme);
+
+
+// Checa el localStorage para mantener el therme
+const temaActual = localStorage.getItem('theme');
+
+if(temaActual){
+
+    document.documentElement.setAttribute('data-theme',temaActual);
+    if(temaActual === 'dark'){
+        toogleSwitch.checked = true;
+        modoOscuro();
+    }
+}
